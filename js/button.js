7fe281f99f3bbe8fce6fd1a83ae1eeaf548afef2
-Kevin_ <t66b6g77@gmail.com>
2025-03-30 14:55:04 -0300

    footer link updated

--- js/button.js
+++
@@ -1,7 +0,0 @@
-$(".label").mousedown(function(e) {
-    $(this).addClass("active");
-}).click(function(e) { // Fixed typo: changed 'mouseclick' to 'click'
-    $(this).removeClass("active");
-}).mouseout(function() {
-    $(this).removeClass("active");
-});
